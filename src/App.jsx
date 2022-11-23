1f800b5a40ea0a3e91a7aa5f7cd17d9c20438b7b
Vipul <vipulbarjatya@gmail.com>
2022-11-23 11:22:06 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import React, { useState } from "react";
+import Counter from "./Counter";
+import CounterHooks from "./CounterHooks";
+
+export const ThemeContext = React.createContext();
+
 function App() {
-  return <h1>Hello World</h1>;
+  console.log("render app");
+  const [theme, setTheme] = useState("red");
+  return (
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      Counter
+      <Counter initialCount={0} />
+      CounterHooks
+      <CounterHooks initialCount={0} />
+      <button
+        onClick={() => {
+          setTheme((prevTheme) => {
+            return prevTheme === "red" ? "blue" : "red";
+          });
+        }}
+      >
+        Toggle Theme
+      </button>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
---
+++ src/CounterHooks.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const CounterHooks = ({ initialCount }) => {
+  const [count, setCount] = useState(initialCount);
+  return (
+    <div>
+      <button
+        onClick={() =>
+          count <= 0
+            ? setCount((prevCount) => prevCount)
+            : setCount((prevCount) => prevCount - 1)
+        }
+      >
+        -
+      </button>
+      <span>{count}</span>
+      <button
+        onClick={() =>
+          count >= 10
+            ? setCount((prevCount) => prevCount)
+            : setCount((prevCount) => prevCount + 1)
+        }
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default CounterHooks;
